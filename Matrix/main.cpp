b164202c4d69d3b7092cdfdb48ba2835307c7911
Miłosz Skobejko <miloszskobejko@icloud.com>
2023-03-18 12:20:50 +0100

    Initial Commit

---
+++ Matrix/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Matrix
+//
+//  Created by Miłosz Skobejko on 18/03/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
